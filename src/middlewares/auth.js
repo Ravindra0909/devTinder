0185f230903cbd714e8cca688d005e14b77b7f05
Ravindra0909 <koppadaravindra@gmail.com>
2026-03-02 12:12:05 +0530

    some silly bugs

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -5,8 +5,7 @@ const userAuth = async (req, res, next) => {
   try {
     const { token } = req.cookies;
     if (!token) {
-      // throw new Error("Token is not Valid");
-      return res.status(401).send("Please Login!!");
+      return res.status(401).send("Please Login  !!");
     }
     const decodedMsg = await jwt.verify(token, "DEVTINDER@SECRET");
 
